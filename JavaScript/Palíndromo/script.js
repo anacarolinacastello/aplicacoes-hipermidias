009cdd9fb50f82611559a8b30033c25a8d4d451d
anacarolinacastello <anacarolinacastellop@gmail.com>
2021-07-21 20:36:47 -0300

    palíndromo

--- JavaScript/Palíndromo/script.js
+++ JavaScript/Palíndromo/script.js
@@ -1,12 +1,15 @@
 let word = prompt('Digite uma palavra:');
 let auxWord = '';
 
-for (let i = word.length - 1; i >= 0; i--) {
-  auxWord += word[i];
+function palindromo(entrada) {
+  for (let i = word.length - 1; i >= 0; i--) {
+    auxWord += word[i];
+  }
+  if (word === auxWord) {
+    return alert('É um Palíndromo');
+  } else {
+    return alert('Não é um Palíndromo!');
+  }
 }
 
-if (word === auxWord) {
-  alert('É um Palíndromo');
-} else {
-  alert('Não é um Palíndromo!');
-}
+palindromo(word);
