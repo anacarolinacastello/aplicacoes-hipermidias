dec784026ccc1003a3dc954129b9ed18171c6d64
anacarolinacastello <anacarolinacastellop@gmail.com>
2021-07-25 16:43:57 -0300

    tempo transcorrido

---
+++ JavaScript/tempo-transcorrido/script.js
@@ -0,0 +1,22 @@
+let dataEntrada = new Date(2010, 07, 14);
+let dataAtual = new Date();
+
+function tempoTranscorrido(a, b) {
+  let tempo = dataAtual - dataEntrada;
+
+  let diasCount = 1000 * 60 * 60 * 24;
+  let mesesCount = diasCount * 30.41666666666667;
+  let anosCount = mesesCount * 12;
+
+  let anos = Math.floor(tempo / anosCount);
+
+  tempo = tempo - anos * anosCount;
+  let meses = Math.floor(tempo / mesesCount);
+  tempo = tempo - meses * mesesCount;
+  let dias = Math.floor(tempo / diasCount);
+
+  return console.log(
+    `Tempo transcorrido: ${anos} anos ${meses + 1} meses ${dias} dias`
+  );
+}
+tempoTranscorrido(dataEntrada, dataAtual);
