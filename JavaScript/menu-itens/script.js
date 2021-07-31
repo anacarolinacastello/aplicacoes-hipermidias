99b67c860b2e3ba96e0fd750ff788c452d9943b3
anacarolinacastello <anacarolinacastellop@gmail.com>
2021-07-31 15:00:30 -0300

    menu-itens

--- JavaScript/menu-itens/script.js
+++ JavaScript/menu-itens/script.js
@@ -1,26 +1,33 @@
 const listContainer = document.querySelector('[data-list]');
-const listInput = document.querySelector('[data-input]');
+const newList = document.querySelector('[data-form]');
+const input = document.querySelector('[data-input]');
 
-let list = [];
-listInput.addEventListener('submit', function (e) {
+let lists = [];
+
+newList.addEventListener('submit', function (e) {
   e.preventDefault();
-  const listName = listInput.value;
-  if (listName === null || listName === '') {
-    return;
-  }
+  const listName = input.value;
+
+  if (listName === null || listName === '') return;
   const list = createList(listName);
-  listInput.value = null;
-  list.push(list);
+  input.value = null;
+  lists.push(list);
   render();
 });
 function createList(name) {
-  return { id: Date.now().toString(), name: name }; //função que retonara um objeto, a montagem de um objeto
+  return { id: Date.now().toString(), name: name };
 }
 function render() {
+  clearElement(listContainer);
   lists.forEach(function (list) {
-    const item = document.createElement('li'); //criar a lista, jogando pro html, criando elemento html
-    item.classList.add('item'); // adiciona uma class, a nossa lista de classes
-    item.innerText = list.name; //recuperar item e dizer que ele vai receber com texto o list
-    listContainer.appendChild(item); //elemento mãe que vai receber as listas, a div fantasma *para passar elemnto filho appendchild, que é o item
+    const item = document.createElement('li');
+    item.classList.add('item');
+    item.innerText = list.name;
+    listContainer.appendChild(item);
   });
+}
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
 }
