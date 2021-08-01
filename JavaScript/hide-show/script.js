c6ab36009c88d4541330d4e9585f795b97c4057a
anacarolinacastello <anacarolinacastellop@gmail.com>
2021-07-31 22:35:09 -0300

    hide-show

--- JavaScript/hide-show/script.js
+++ JavaScript/hide-show/script.js
@@ -2,29 +2,29 @@ function show() {
   let paragraph = document.getElementById('p');
   if (paragraph.style.display === 'none') {
     paragraph.style.display = 'block';
-    document.getElementById('a1').innerHTML = 'Ocultar Conteudo';
+    document.getElementById('a1').innerHTML = 'Ocultar Contéudo';
   } else {
     paragraph.style.display = 'none';
-    document.getElementById('a1').innerHTML = 'Mostrar Conteudo';
+    document.getElementById('a1').innerHTML = 'Mostrar Contéudo';
   }
 }
 function showa() {
   let paragraph = document.getElementById('a');
   if (paragraph.style.display === 'none') {
     paragraph.style.display = 'block';
-    document.getElementById('a2').innerHTML = 'Mostrar Conteudo';
+    document.getElementById('a2').innerHTML = 'Ocultar Contéudo';
   } else {
     paragraph.style.display = 'none';
-    document.getElementById('a2').innerHTML = 'Ocultar Conteudo';
+    document.getElementById('a2').innerHTML = 'Mostrar Contéudo';
   }
 }
 function showb() {
   let paragraph = document.getElementById('b');
   if (paragraph.style.display === 'none') {
     paragraph.style.display = 'block';
-    document.getElementById('a3').innerHTML = 'Mostrar Conteudo';
+    document.getElementById('a3').innerHTML = 'Ocultar Conteudo';
   } else {
     paragraph.style.display = 'none';
-    document.getElementById('a3').innerHTML = 'Ocultar Conteudo';
+    document.getElementById('a3').innerHTML = 'Mostrar Contéudo';
   }
 }
