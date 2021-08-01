0007b86685cc543cf126837638cdb152a50a8b97
anacarolinacastello <anacarolinacastellop@gmail.com>
2021-07-31 23:15:12 -0300

    lampada

---
+++ JavaScript/lampada/script.js
@@ -0,0 +1,8 @@
+function luz() {
+  let on = document.getElementById('on');
+  if (on.style.display === 'none') {
+    on.style.display = 'block';
+  } else {
+    on.style.display = 'none';
+  }
+}
